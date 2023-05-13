4eba82efbc0eb6ed20b4f7e46f493eb0dd852c96
LucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2023-05-13 19:11:03 -0300

    ss

--- api/src/db.js
+++ api/src/db.js
@@ -3,15 +3,16 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,DB_PORT,
 } = process.env;
 let sequelize = 
+
 process.env.NODE_ENV ==="production"?
   new Sequelize({
     database: DB_NAME,
     dialect: "postgres", 
     host: DB_HOST,
-    port: 8057,
+    port: DB_PORT,
     username: DB_USER,
     password: DB_PASSWORD,
     pool:{
